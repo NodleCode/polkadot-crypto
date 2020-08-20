6e8ac41f722c0f3ad7e47dee1da396021bd88b6a
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-08-20 09:44:35 -0700

    copy shims

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// Copyright 2019-2020 @polkadot/wasm-crypto authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+pub mod bip39;
+pub mod ed25519;
+pub mod hashing;
+pub mod sr25519;
+
+fn main() {
+    println!("test");
+}
